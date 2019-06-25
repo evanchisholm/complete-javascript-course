56d172a90be6d79fbf098f68d0acb806d4636e27
evanchisholm <evan_chisholm@yahoo.co.uk>
2019-06-25 20:54:23 +1000

    section 3 complete

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,19 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/*calculateAge(1968);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+}
 
+// error if before function expression
+// retirement(1967);
 
+var retirement = function(year) {
+    console.log(65 - (2019 - year));
+}
 
+retirement(1967);
 
+// variables
+// variable is known but value is not set yet, so undefined
+console.log(age);
+// Global scope variable
+var age = 23;
 
-
-
-
-
-
-
-
+function foo() {
+    // function scope variable
+    var age = 65
+    console.log(age);
+}
+foo();
+console.log(age);*/
 
 
 
@@ -40,7 +55,7 @@ function first() {
 
 
 
-// Example to show the differece between execution stack and scope chain
+// Example to show the difference between execution stack and scope chain
 
 /*
 var a = 'Hello!';
@@ -67,11 +82,39 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
 
+/*
+calculateAge(1984);
 
+function calculateAge(year) {
+    console.log(2019 - year);
+    console.log(this);
+}
+*/
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
 
+        /*function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();*/
+    }
+};
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
+// method borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
