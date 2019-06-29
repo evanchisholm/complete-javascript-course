d67daff0987ee887f3bd1ca8fb00c666eba44ab9
evanchisholm <evan_chisholm@yahoo.co.uk>
2019-06-29 15:37:51 +1000

    section 5 closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -181,6 +181,7 @@ game();*/
 
 // Closures
 
+/*
 function retirement(retirementAge) {
     var a = ' years left until retirement.';
     return function (yearOfBirth) {
@@ -196,12 +197,47 @@ var retirementIceland = retirement(67);
 retirementGermany(1990);
 retirementUS(1990);
 retirementIceland(1990);
+*/
 
 // retirement(65)(1967);
 
+/*function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}*/
 
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
+interviewQuestion('teacher')('Ray');
 
+var interviewTeacher = interviewQuestion('teacher');
+var interviewDesigner = interviewQuestion('designer');
+var interviewFireman = interviewQuestion('fireman');
+
+interviewTeacher('Evan');
+interviewDesigner('Mark');
+interviewFireman('Brad');
 
 
 
